75a47d25be6445fa2736a7d09e14c30f0d8f5db6
CSCI4300GroupHub <102686806+CSCI4300GroupHub@users.noreply.github.com>
2022-04-04 19:37:31 -0400

    Add files via upload

---
+++ src/aboutPage.php
@@ -0,0 +1,49 @@
+<?php
+include_once('connection.php');
+?>
+<!DOCTYPE html>
+<html lang="en" class="background">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopster | Log in</title>
+    <link href="./style/main.css" rel="stylesheet">
+    <link href="./style/misc-style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <a href="index.php"><span><h1 class ="logo">shopster.</h1></span></a>
+        <div class="navbar" id="navbarNavAltMarkup">
+            <ul>
+                <li><a class="button-header" href="./index.php"><i>home</a></li>
+                <li><a class="button-header" href="./productPage.php">products</a></li>
+                <li><a class="button-header" href="./aboutPage.php">about</i></a></li>
+                <?php
+                 if(isset($_SESSION['logged_in'])&&$_SESSION["logged_in"]=true){
+                    echo '<li style="float:right"><a class="active" href="./logout.php">Log Out</a></li>';
+                 }
+                 else{
+                    echo '<li style="float:right"><a class="active" href="./loginPage.php">Log In</a></li>';
+                 }
+                ?>
+            </ul>
+        </div>
+    </nav>
+    <div>
+        <div class="todayHeader">
+            <p>-   ABOUT US   -</p>
+        </div>
+    </div>
+
+</body>
+
+<footer>
+    <div style="color:white;">
+        Shopster &copy; 2022
+    </div>
+</footer>
+
+</html>
