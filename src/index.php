6f6479f579e6836ca4e70996a1213d8bf6dfb72d
CSCI4300GroupHub <102686806+CSCI4300GroupHub@users.noreply.github.com>
2022-04-04 15:48:14 -0400

    Update index.php
    update page css and more products added

--- src/index.php
+++ src/index.php
@@ -17,7 +17,7 @@
 <body>
     <nav>
         <a href="index.php"><span><h1 class ="logo">shopster.</h1></span></a>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar" id="navbarNavAltMarkup">
             <ul>
                 <li><a class="button-header" href="./index.php">home</a></li>
                 <li><a class="button-header" href="#news">products</a></li>
@@ -43,57 +43,85 @@
             <p>-   TODAY'S DEALS   -</p>
         </div>
         <div class="itemDisplayContainer">
-            <div class="dealItem">
-                <img src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/a03158c1-8969-4e5f-acea-37eef71cf0b7/air-jordan-1-retro-high-og-shoe-PLe8kL.png" alt="">
+             <div class="dealItem">
+                <a href=""><img  src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/a03158c1-8969-4e5f-acea-37eef71cf0b7/air-jordan-1-retro-high-og-shoe-PLe8kL.png" alt=""></a>
                 <p class="product-text">Product</p>
                 <a href="" class="button1">
                     Add to cart
                 </a>
             </div>
             <div class="dealItem">
-                <img src="https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/953f8c77-48ab-4583-b040-c04a3a93ab32/air-jordan-1-ko-chicago-release-date.jpg" alt="">
+                <a href=""><img src="https://static.nike.com/a/images/t_prod_ss/w_960,c_limit,f_auto/953f8c77-48ab-4583-b040-c04a3a93ab32/air-jordan-1-ko-chicago-release-date.jpg" alt=""></a>
                 <p class="product-text">Product</p>
                 <a href="" class="button1">
                     Add to cart
                 </a>
             </div>
             <div class="dealItem">
-                <img src="https://www.highsnobiety.com/static-assets/thumbor/g9qmlsrSzNY4d55HaM3HxiNa5Rs=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2021/12/01120911/ama-aj1-2.jpeg" alt="">
+                <a href=""><img src="https://www.highsnobiety.com/static-assets/thumbor/g9qmlsrSzNY4d55HaM3HxiNa5Rs=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2021/12/01120911/ama-aj1-2.jpeg" alt=""></a>
                 <p class="product-text">Product</p>
                 <a href="" class="button1">
                     Add to cart
                 </a>
             </div>
             <div class="dealItem">
-                <img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt="">
+                <a href=""><img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt=""></a>
                 <p class="product-text">Product</p>
                 <a href="" class="button1">
                     Add to cart
                 </a>
             </div>
             <div class="dealItem">
-                <img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt="">
+                <a href=""><img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt=""></a>
                 <p class="product-text">Product</p>
                 <a href="" class="button1">
                     Add to cart
                 </a>
             </div>
             <div class="dealItem">
-                <img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt="">
+                <a href=""><img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt=""></a>
                 <p class="product-text">Product</p>
                 <a href="" class="button1">
                     Add to cart
                 </a>
             </div>
             <div class="dealItem">
-                <img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt="">
+                <a href=""><img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt=""></a>
                 <p class="product-text">Product</p>
                 <a href="" class="button1">
                     Add to cart
                 </a>
             </div>
             <div class="dealItem">
-                <img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt="">
+                <a href=""><img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt=""></a>
+                <p class="product-text">Product</p>
+                <a href="" class="button1">
+                    Add to cart
+                </a>
+            </div>
+            <div class="dealItem">
+                <a href=""><img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt=""></a>
+                <p class="product-text">Product</p>
+                <a href="" class="button1">
+                    Add to cart
+                </a>
+            </div>
+            <div class="dealItem">
+                <a href=""><img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt=""></a>
+                <p class="product-text">Product</p>
+                <a href="" class="button1">
+                    Add to cart
+                </a>
+            </div>
+            <div class="dealItem">
+                <a href=""><img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt=""></a>
+                <p class="product-text">Product</p>
+                <a href="" class="button1">
+                    Add to cart
+                </a>
+            </div>
+            <div class="dealItem">
+                <a href=""><img src="https://www.highsnobiety.com/static-assets/thumbor/Z5LvtQYYpR69OwFjtuFbXF1zboY=/1600x1067/www.highsnobiety.com/static-assets/wp-content/uploads/2020/04/02175908/nike-air-jordan-1-mid-light-ash-release-date-price-02.jpg" alt=""></a>
                 <p class="product-text">Product</p>
                 <a href="" class="button1">
                     Add to cart
