9fe1cbdcedce51f0c39245c7bb6e3572320081bb
tomatooy <ouyangzongxin@gmail.com>
2022-12-08 21:55:54 -0500

    search

--- src/movieSearch.php
+++ src/movieSearch.php
@@ -1,9 +1,6 @@
 <?php
-//include class
-session_start();
+require_once('connection.php');
 require_once('./php/component.php');
-require_once('./connection.php');
-$database = DBConnection::get_instance();
 try {
     $dsn = 'mysql:host=localhost;dbname=mycinema';
     $username = 'root';
@@ -13,27 +10,24 @@ try {
     $error_message = $e->getMessage();
     echo "<p> An error occured while connecting to the database: $error_message </p>";
 }
-$date = date('Y-m-d');
+if(isset($_POST['search'])){
+    $word = "%".htmlspecialchars($_POST['search'])."%";   
+}
+
 //Product of a category
-$queryProducts = 'SELECT * FROM  all_movie where `date` > "' . $date . '" ORDER BY `date` ASC';
-$statement = $db->prepare($queryProducts);
+$querymovies = "SELECT * FROM  `all_movie` where `movie_name` like '".$word."' " ;
+$statement = $db->prepare($querymovies);
 $statement->execute();
 $movies = $statement->fetchAll();
 $statement->closeCursor();
-
 ?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="./assets/images/icon.png" type="image/png">
-    <title>myCinema</title>
+    <title>Search</title>
     <link href="https://fonts.googleapis.com/css?family=Inter:100,200,300,regular,500,600,700,800,900" rel="stylesheet" />
 
     <link rel="stylesheet" href="./assets/css/main.css">
@@ -56,30 +50,27 @@ $statement->closeCursor();
             border: 1px solid white;
             padding: 3px;
             border-radius: 2px;
-            text-align: center;
+            text-align:center;
 
         }
-
-        .cardText {
+        .cardText{
             position: absolute;
             color: white;
             z-index: 21;
-            bottom: 0;
-            left: 50%;
+            bottom:0;
+            left:50%;
             transform: translate(-50%, -50%);
             opacity: 1;
-            text-align: center;
+            text-align:center;
         }
 
         .category-card:hover .bookLink {
             opacity: 1;
         }
-
-        .category-card:hover .cardText {
+        .category-card:hover .cardText{
             opacity: 0;
             display: none;
         }
-
         .link {
             position: absolute;
             width: 100%;
@@ -90,13 +81,8 @@ $statement->closeCursor();
         }
     </style>
 </head>
-
 <body>
-    <a class="CartIcon" href="./checkOutPage.php">Check Out</a>
-
-    <div class="container">
-
-        <header class="">
+<header class="">
             <div class="navbar">
 
 
@@ -109,12 +95,14 @@ $statement->closeCursor();
                 <nav class="">
                     <ul class="navbar-nav">
 
-                        <li> <a href="#" class="navbar-link">Home</a> </li>
-                        
+                        <li> <a href="./index.php" class="navbar-link">Home</a> </li>
+                        <li> <a href="#category" class="navbar-link">Category</a> </li>
                         <?php
-                        if (isset($_SESSION['admin']) && $_SESSION["admin"] = true) {
+                        if(isset($_SESSION['admin']) && $_SESSION["admin"] = true){
                             echo "<li style='margin:center'><a href='./adminMain.php' class='userHello'>Admin</i></a></li>";
-                        } else if (isset($_SESSION['logged_in']) && $_SESSION["logged_in"] = true) {
+
+                        }
+                        else if(isset($_SESSION['logged_in']) && $_SESSION["logged_in"] = true) {
                             echo "<li style='margin:center'><a href='./userPortalMain.php' class='userHello'>Hello, " . $_SESSION['username'] . "</i></a></li>";
                         }
                         ?>
@@ -124,7 +112,7 @@ $statement->closeCursor();
 
                 <div class="navbar-actions">
 
-                    <form action="movieSearch.php" class="navbar-form" method="post">
+                    <form action="#" class="navbar-form">
                         <input type="text" name="search" placeholder="I'm looking for..." class="navbar-form-search">
 
                         <button class="navbar-form-btn">
@@ -156,72 +144,25 @@ $statement->closeCursor();
 
             </div>
         </header>
-        <main>
-            <section class="banner">
-                <div class="banner-card">
-
-                    <!-- <img src="./assets/images/John-Wick-3.jpg" class="banner-img" alt=""> -->
-                    <iframe src="https://www.youtube.com/embed/M7XM597XO94" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-
-                    <div class="card-content">
-
-                        <h2 class="card-title">John Wick: Chapter 3 - Parabellum</h2>
-                    </div>
-
-                </div>
-            </section>
-         
-            <section class="category" id="category">
-
-                <h2 class="section-heading">Upcoming</h2>
-
-                <div class="category-grid">
-                    <?php foreach ($movies as $movie) : ?>
+        <section class="category" id="category">
+        <div class="category-grid">
+        <?php foreach ($movies as $movie) : ?>
                         <?php echo '
                                 <div class="category-card">
-                                <img src="' . $movie['image'] . '" alt="" class="card-img">
+                                <img src="'.$movie['image'].'" alt="" class="card-img">
                                 <div class="bookLink">
-                                    <a href="./bookingTicket.php?id=' . $movie['id'] . '&time=' . $movie['date'] . '&theatre=">
+                                    <a href="./bookingTicket.php?id='.$movie['id'].'&time='.$movie['date'].'&theatre=">
                                     Book Movie
                                     </a>
                                 </div>
                                 <div class="cardText">
-                                    <div>' . $movie['movie_name'] . '</div>
-                                    <div>' . $movie['date'] . '</div>
+                                    <div>'.$movie['movie_name'].'</div>
+                                    <div>'.$movie['date'].'</div>
                                 </div>
                                 </div>'
                         ?>
-                    <?php endforeach; ?>
-                </div>
-            </section>
-
-            <section class="category" id="category">
-                <h2 class="section-heading">Currently Showing</h2>
-                <div class="category-grid">
-                    <div class="category-card">
-                        <img src="./assets/images/adventure.jpg" alt="" class="card-img">
-                    </div>
-
-                    <div class="category-card">
-                        <img src="./assets/images/animated.jpg" alt="" class="card-img">
-                    </div>
-
-                    <div class="category-card">
-                        <img src="./assets/images/crime.jpg" alt="" class="card-img">
-                    </div>
-                    <div class="category-card">
-                        <img src="./assets/images/sci-fi.jpg" alt="" class="card-img">
-                    </div>
-                </div>
-            </section>
-    </div>
-
-
-
-
-    <script src="./assets/js/main.js"></script>
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+                <?php endforeach; ?>
+        </div>
+        </section>
 </body>
-
 </html>
